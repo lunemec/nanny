6abbc49fa186085cc409da10aa33ec67aa14f017
Lukas Nemec <lukas.nemec@nic.cz>
2018-06-29 15:42:01 +0200

    Version 0.3.

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // Version is, suprisingly nanny version.
-const Version = "0.2"
+const Version = "0.3"
 
 // VersionString to have it nice.
 const VersionString = "Nanny v" + Version
